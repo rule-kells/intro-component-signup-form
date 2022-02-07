6745d5a15a9f2f6c9cfd26c08054c75f52bbc075
Kellen Ruyle <42897132+rule-kells@users.noreply.github.com>
2022-02-07 11:13:38 -0600

    loaded initial intro-component-signup-form files

--- js/main.js
+++ js/main.js
@@ -1,54 +1,51 @@
 const form = document.getElementById('form')
-// const inputs = Array.from(form.querySelectorAll('input'))
-const firstName = document.getElementById('first-name')
-const lastName = document.getElementById('last-name')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-// const submit = document.querySelector('.submit')
+const firstName = form.querySelector('.first-name')
+const lastName = form.querySelector('.last-name')
+const email = form.querySelector('.email')
+const password = form.querySelector('.password')
+const submit = form.querySelector('.submit')
 
+submit.addEventListener('click', (ev) => {
+  ev.preventDefault()
+
+  checkInputs()
+})
 
 function checkInputs() {
+  const firstNameValue = firstName.value.trim()
+  const lastNameValue = lastName.value.trim()
+  const emailValue = email.value.trim()
+  const passwordValue = password.value.trim()
 
-
-  const firstNameValue = firstName.value
-  const lastNameValue = lastName.value
-  const emailValue = email.value
-  const passwordValue = password.value
-
- const validate = document.querySelector('input').validity.valid
-
-  if (firstNameValue !== validate) {
+  if (firstNameValue === '') {
     addErrorMsg(firstName)
   } else {
     removeErrorMsg(firstName)
   }
 
-  if (lastNameValue !== validate) {
+  if (lastNameValue === '') {
     addErrorMsg(lastName)
   } else {
     removeErrorMsg(lastName)
   }
 
-  if (emailValue !== validate) {
+  if (!validateEmail(emailValue)) {
     addErrorMsg(email)
   } else {
     removeErrorMsg(email)
   }
 
-  if (passwordValue !== validate) {
+  if (passwordValue === '') {
     addErrorMsg(password)
   } else {
     removeErrorMsg(password)
   }
-
- 
 }
 
 function addErrorMsg(input) {
   const formDiv = input.parentElement
   const errorMsg = formDiv.querySelector('.error-msg-hidden')
 
-  
   errorMsg.classList.add('error-msg')
   input.classList.add('error')
 }
@@ -56,18 +53,13 @@ function addErrorMsg(input) {
 function removeErrorMsg(input) {
   const formDiv = input.parentElement
   const errorMsg = formDiv.querySelector('.error-msg-hidden')
-
+  
   errorMsg.classList.remove('error-msg')
   input.classList.remove('error')
 }
 
-// function validate(validateValue) {
-//    validateValue = document.querySelector('input').validity.valid
-//   return validateValue
-// }
-
-form.addEventListener('submit', function(ev) {
-  ev.preventDefault()
-
-  checkInputs()
-})
+function validateEmail(validateValue) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    validateValue,
+  )
+}
